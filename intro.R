5323baa6180f9c7142b4720765529f3f02a78413
Kalicz Péter <kaliczp@gmail.com>
2024-03-20 11:22:12 +0100

    Combine filters

--- intro.R
+++ intro.R
@@ -8,3 +8,4 @@ print(las)
 las <- readLAS("RAW_LiDAR_01.las", select = "xyz")
 ## ugyan az, mint a print las
 las
+las <-  readLAS("RAW_LiDAR_01.las", filter = "-keep_first -drop_z_below 170 -drop_z_above 200")
