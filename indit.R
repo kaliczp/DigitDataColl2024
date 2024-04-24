498463a2b169177bfe7f4135a8b8dba2089f8297
Kalicz Péter <kaliczp@gmail.com>
2024-04-24 09:47:27 +0200

    crossection function added

--- indit.R
+++ indit.R
@@ -3,3 +3,19 @@ library(lidR)
 library(ggplot2)
 LASfile <- system.file("extdata", "Topography.laz", package="lidR")
 las <- readLAS(LASfile, select = "xyzrn")
+
+## Function
+plot_crossection <- function(las,
+                             p1 = c(min(las@data$X), mean(las@data$Y)),
+                             p2 = c(max(las@data$X), mean(las@data$Y)),
+                             width = 4, colour_by = NULL)
+{
+  colour_by <- rlang::enquo(colour_by)
+  data_clip <- clip_transect(las, p1, p2, width)
+  p <- ggplot(data_clip@data, aes(X,Z)) + geom_point(size = 0.5) + coord_equal() + theme_minimal()
+
+  if (!is.null(colour_by))
+    p <- p + aes(color = !!colour_by) + labs(color = "")
+
+  return(p)
+}
