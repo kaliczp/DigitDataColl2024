771f899ec8396d170bea4af26768b134b3f87096
Kalicz Péter <kaliczp@gmail.com>
2024-05-08 10:41:36 +0200

    generate tin

--- DTM.R
+++ DTM.R
@@ -2,3 +2,7 @@
 LASfile <- system.file("extdata", "Topography.laz", package="lidR")
 las <- readLAS(LASfile, select = "xyzc")
 plot(las, size = 3, bg = "white")
+
+##
+dtm_tin <- rasterize_terrain(las, res = 1, algorithm = tin())
+plot_dtm3d(dtm_tin, bg = "white") 
