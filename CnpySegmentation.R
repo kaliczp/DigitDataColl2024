21ac9bd5948d75ff25d301da7ecdc5203e921f7f
Kalicz Péter <kaliczp@gmail.com>
2024-06-28 17:20:10 +0200

    Local max filter

--- CnpySegmentation.R
+++ CnpySegmentation.R
@@ -5,3 +5,10 @@ LASfile <- system.file("extdata", "MixedConifer.laz", package="lidR")
 las <- readLAS(LASfile, select = "xyzr", filter = "-drop_z_below 0")
 chm <- rasterize_canopy(las, 0.5, pitfree(subcircle = 0.2))
 plot(las, bg = "white", size = 4)
+
+### ITD
+## Local max filter
+library(sf)
+ttops <- locate_trees(las, lmf(ws = 5))
+plot(chm, col = height.colors(50))
+plot(sf::st_geometry(ttops), add = TRUE, pch = 3)
